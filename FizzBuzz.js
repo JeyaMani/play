3a35f27dc37f95771e983bb5e60d3aa1599dec80
Jeya Thangapandi <tjeya26@gmail.com>
2018-12-17 15:45:08 -0800

    Fixed the comments.

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,5 +1,32 @@
 #!/usr/bin/env node
+/*
+Write a program that outputs the string representation of numbers from 1 to n.
 
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+
+Example:
+
+n = 15,
+
+Return:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+]
+*/
 if(process.argv[2] === undefined || !process.argv[2].match(/^[0-9]+$/)) {
     console.log("Provided '" + process.argv[2] + "'");
     console.log("Error: you must provide an integer.");
