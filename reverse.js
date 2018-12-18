af8ff9e12deb2dd38b7d8a7d477d107e85bd5bfd
jeyalakshmi Thangapandi <jeyalakshmithangapandi@jeyalakshmis-MacBook-Pro.local>
2018-12-18 13:12:21 -0800

    Fixed the commants.

--- reverse.js
+++ reverse.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
+/*
+Write a function that takes a string as input and returns the string reversed.
+
+Example 1:
+
+Input: "hello"
+Output: "olleh"
+Example 2:
+
+Input: "A man, a plan, a canal: Panama"
+Output: "amanaP :lanac a ,nalp a ,nam A"
+*/
 
 function reverse(str)
 {
