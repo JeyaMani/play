06687347b977da0a96ac571cfbd272ff8a829243
Jeya Thangapandi <tjeya26@gmail.com>
2018-12-17 16:02:29 -0800

    Fixed the comments.

--- PerfectSquare.js
+++ PerfectSquare.js
@@ -1,5 +1,18 @@
 #!/usr/bin/env node
+/*
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
 
+Note: Do not use any built-in library function such as sqrt.
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 14
+Output: false
+*/
 if(process.argv[2] === undefined || !process.argv[2].match(/^[0-9]+$/)) {
     console.log("Provided '" + process.argv[2] + "'");
     console.log("Error: you must provide an integer.");
