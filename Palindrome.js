70dbdf805b5aaffa44329b99b1a94cd2121da734
Jeya Thangapandi <tjeya26@gmail.com>
2019-01-03 15:22:27 -0800

    Fixed the comments.

--- Palindrome.js
+++ Palindrome.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
+/*
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+*/
 
 var n_array= [];
 for(var a= 2; a < process.argv.length; a++) {
