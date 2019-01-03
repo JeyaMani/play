bac441868aed9c45d71f37c9b31a362d362c7079
Jeya Thangapandi <tjeya26@gmail.com>
2019-01-03 14:18:17 -0800

    Fixed the comments and deleted some old commented-out code.

--- Contains-Duplicate.js
+++ Contains-Duplicate.js
@@ -1,6 +1,24 @@
 #!/usr/bin/env node
+/*
+Given an array of integers, find if the array contains any duplicates.
 
-//function containsDuplicate(arr) {
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+Example 2:
+
+Input: [1,2,3,4]
+Output: false
+Example 3:
+
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+*/
+
+function containsDuplicate(arr) {
 var arr = [1,2,3,4,5,2,1];
 var sorted_arr = arr.slice().sort(); 
 var results = [];
@@ -9,19 +27,5 @@ for (var i = 0; i < sorted_arr.length - 1; i++) {
         results.push(sorted_arr[i]);
     }
 }
-//}
-console.log(results);
-
-
-/*
-var arr = [1, 1, 4, 5, 5], dup = [], duplicates = [];
-
-for(var i = 0; i < arr.length; i++){
-  if(dup.includes(arr[i]) && !duplicates.includes(arr[i]))
-    duplicates.push(arr[i])
-  else
-    dup.push(arr[i]);
 }
-
-console.log(duplicates);
-*/
+console.log(results);
