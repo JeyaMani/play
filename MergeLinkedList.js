53ace4ed3176ab8a8813569b10a1d083244552df
Jeya Thangapandi <tjeya26@gmail.com>
2019-01-03 15:04:41 -0800

    Fixed the comments.

--- MergeLinkedList.js
+++ MergeLinkedList.js
@@ -1,4 +1,13 @@
 #!/usr/bin/env node
+/*
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+*/
+
 function mergeTwoSortedLists (l1, l2) {
 var mergedLinkedListHead = { val : -1, next : null }; 
 var runner = mergedLinkedListHead;
@@ -16,3 +25,4 @@ runner.next = l1;
 runner.next = l1 || l2;
 return mergedLinkedListHead.next;
 }
+console.log(mergeTwoSortedLists([1->2->4, 1->3->4]));
