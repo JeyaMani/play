92291b9b34832c699927b821c3f00df27c53c7ce
Jeya Thangapandi <tjeya26@gmail.com>
2019-01-03 14:56:49 -0800

    Fixed the comments.

--- MajorityElement.js
+++ MajorityElement.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
+/*
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+*/
 
 var majorityElement = function(nums) {
     var counts = {}, majority = {count:0, number:0};
@@ -13,4 +27,4 @@ var majorityElement = function(nums) {
     
     return parseInt(majority.number);
 };
-console.log(majorityElement(1,2,3,2,2,2,1,5));
+console.log(majorityElement(3,2,3));
