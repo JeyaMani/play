b3a3d199cbdabbbcc42e3c8c506d4bbbbe099903
Jeya Thangapandi <tjeya26@gmail.com>
2019-01-03 15:30:01 -0800

    Fixed the comments.

--- PerfectSquare.js
+++ PerfectSquare.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
+/*
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+Example 1:
+
+Input: n = 12
+Output: 3 
+Explanation: 12 = 4 + 4 + 4.
+Example 2:
+
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+*/
 
 if(process.argv[2] === undefined || !process.argv[2].match(/^[0-9]+$/)) {
     console.log("Provided '" + process.argv[2] + "'");
