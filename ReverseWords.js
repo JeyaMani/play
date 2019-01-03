405e44023ef47f122f651c9f6b751e043e004f9a
Jeya Thangapandi <tjeya26@gmail.com>
2019-01-03 15:51:34 -0800

    Fixed the comments.

--- ReverseWords.js
+++ ReverseWords.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
+/*
+Write a function that takes a string as input and returns the string reversed.
+
+Example 1:
+
+Input: "hello"
+Output: "olleh"
+Example 2:
+
+Input: "A man, a plan, a canal: Panama"
+Output: "amanaP :lanac a ,nalp a ,nam A"
+*/
 
 if(process.argv[2] === undefined) {
     console.log("Error: you must provide a string.");
