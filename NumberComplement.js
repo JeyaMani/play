d2787e49700f39b270467c3bf39094af087aa38c
Jeya Thangapandi <tjeya26@gmail.com>
2019-01-03 15:10:11 -0800

    Fixed the comments.

--- NumberComplement.js
+++ NumberComplement.js
@@ -1,8 +1,23 @@
 #!/usr/bin/env node
+/*
+Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+
+Note:
+The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+You could assume no leading zero bit in the integer’s binary representation.
+Example 1:
+Input: 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+Example 2:
+Input: 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+*/
 
 var findComplement = function(num) {
   const bits = num.toString(2)
   const mask = parseInt("1".repeat(bits.length), 2)
   return num ^ mask
 }
-console.log(findComplement(15));
+console.log(findComplement(1));
